ebb6e4c8fe1699fadeb75b3dd9d2dec97649d096
dewei zhang <377805163@qq.com>
2021-04-22 20:25:44 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import tx from './tx.jpg'
 
 function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  var element = document.createElement('div');
+  element.classList.add('hello');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  const myIcon = new Image();
+  myIcon.src = tx;
+
+  element.appendChild(myIcon);
+
+
+  return element;
+}
+
+document.body.appendChild(component());
